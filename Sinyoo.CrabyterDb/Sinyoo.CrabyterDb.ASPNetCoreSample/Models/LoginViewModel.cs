b81a51726cc734eeb33fb23914ceabe9529ae1be
Stanley Xiao <wisecloud@hotmail.com>
2017-04-25 17:37:30 +0800

    添加示例网站

---
+++ Sinyoo.CrabyterDb/Sinyoo.CrabyterDb.ASPNetCoreSample/Models/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sinyoo.CrabyterDb.ASPNetCoreSample.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "用户名")]
+        [EmailAddress]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "密码")]
+        public string Password { get; set; }
+
+        [Display(Name = "记住我?")]
+        public bool RememberMe { get; set; }
+    }
+}
